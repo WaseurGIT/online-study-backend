253ac4e58ce0dc807e8dd7349f4ce2c9ae65d640
WaseurGIT <waseurrahman31@gmail.com>
2025-12-29 20:55:23 +0600

    added the routes for assignment collection

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+// Environment variables
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+// Express & middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.febqytm.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +25,41 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // collections
+    const assignmentsCollection = client
+      .db("onlineStudyDB")
+      .collection("assignments");
+    const submissionsCollection = client
+      .db("onlineStudyDB")
+      .collection("submissions");
+
+    //   ************ assignment related APIs **************
+
+    // 2. get all assignments from the database
+    app.get("/assignments", async (req, res) => {
+      const cursor = assignmentsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // 1. get the assignment from the client side
+    app.post("/assignments", async (req, res) => {
+      const newAssignment = req.body;
+      const result = await assignmentsCollection.insertOne(newAssignment);
+      res.send(result);
+    });
+
+    // 3. delete an assignment
+    app.delete("/assignments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const cursor = await assignmentsCollection.deleteOne(query);
+      res.send(cursor);
+    });
+
+    // ********* submission related APIs **************
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
